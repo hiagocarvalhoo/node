985e09967a2f7b47f9af7adc13671d053b4f9518
Orion Teles de Mesquita <orionteles@gmail.com>
2024-03-08 19:21:10 -0300

    Lista1

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
 app.get('/', function(req, res){
     res.send('Rota Principal')
 })
@@ -11,6 +14,38 @@ app.get('/hello', function(req, res){
 
 app.get('/nome', function(req, res){
     res.send('Orion Teles de Mesquita')
+})
+
+app.post('/lista1/ex1', function(req, res){
+
+    const total = Number(req.body.total)
+    const brancos = Number(req.body.brancos)
+    const nulos = Number(req.body.nulos)
+    const validos = Number(req.body.validos)
+
+    const soma = brancos + nulos + validos
+    
+    let retorno = {}
+    if(soma > total){
+
+        retorno = {
+            codigo: 'SOMA_ELEITORES',
+            mensagem: "A soma dos votos não pode passar o total de eleitores" 
+        }
+
+        res.status(400).json(retorno)
+        
+    } else {
+        const percBranco = brancos / total * 100
+        const percNulos = nulos / total * 100
+        const percValidos = validos / total * 100
+
+        retorno = {percBranco, percNulos, percValidos}
+
+        res.status(200).json(retorno)
+
+    }
+
 })
 
 app.listen(3000, function(){
