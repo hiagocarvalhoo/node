38048f4b87fbeb310894142a91fface13a4ca64e
Orion Teles de Mesquita <orionteles@gmail.com>
2024-03-13 19:51:11 -0300

    Lista 3

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ app.use(express.urlencoded({extended: false}))
 const introducao = require('./routes/introducao')
 const lista1 = require('./routes/lista1')
 const lista2 = require('./routes/lista2')
+const lista3 = require('./routes/lista3')
 
 app.use('/', introducao)
 app.use('/lista1', lista1)
 app.use('/lista2', lista2)
+app.use('/lista3', lista3)
 
 app.listen(3000, function(){
     console.log('Server UP port 3000')
---
+++ routes/lista3.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+router.post('/ex4', function(req, res){
+
+    res.json({})
+})
+
+module.exports = router
